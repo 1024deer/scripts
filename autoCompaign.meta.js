1011cabea51d47d078f29dab4091b0d710f93bae
1024deer <72494945+1024deer@users.noreply.github.com>
2024-10-25 18:37:48 +0800

    Create autoCompaign.meta.js

---
+++ autoCompaign.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         自动营销活动
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       keven
+// @match        https://seller.tiktokglobalshop.com/promotion/**
+// @match        https://seller.tiktokglobalshop.com/promotion/campaign/detail/**
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/1024deer/scripts/main/autoCompaign.meta.js
+// @downloadURL  https://raw.githubusercontent.com/1024deer/scripts/main/autoCompaign.js
+// ==/UserScript==
