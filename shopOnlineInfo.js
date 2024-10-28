d0ae189ec0d93381085d8e20a1fe0fd74722c992
1024deer <72494945+1024deer@users.noreply.github.com>
2024-10-28 10:55:55 +0800

    Update shopOnlineInfo.js

--- shopOnlineInfo.js
+++ shopOnlineInfo.js
@@ -2,7 +2,7 @@
 
     // 添加Element-UI样式
     GM_addStyle(`
-        @import url('https://cdnjs.cloudflare.com/ajax/libs/element-ui/2.15.6/theme-chalk/index.css');
+        @import url('https://raw.githubusercontent.com/1024deer/scripts/main/elementui.css');
     `);
 
     // 添加自定义样式
