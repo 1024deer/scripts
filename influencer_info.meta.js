61077ab9228d8451a4dd3cceeeef62dbe2af4e17
1024deer <72494945+1024deer@users.noreply.github.com>
2025-05-07 19:16:56 +0800

    Create influencer_info.meta.js

---
+++ influencer_info.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         表格数据悬浮窗
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  在网页右侧添加可拖动的悬浮窗，点击显示表格数据
+// @author       You
+// @match        https://affiliate.tiktokglobalshop.com/connection/creator/detail*
+// @grant        GM_addStyle
+// ==/UserScript==
