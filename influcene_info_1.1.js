0bbf37bf6fbd523c49c029510d805b2407fdd695
1024deer <72494945+1024deer@users.noreply.github.com>
2025-05-07 19:13:32 +0800

    Update influcene_info_1.1.js

--- influcene_info_1.1.js
+++ influcene_info_1.1.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         表格数据悬浮窗
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  在网页右侧添加可拖动的悬浮窗，点击显示表格数据
+// @author       You
+// @match        https://affiliate.tiktokglobalshop.com/connection/creator/detail*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+  'use strict';
 
   // 添加CSS样式
   GM_addStyle(`
@@ -294,7 +306,7 @@
       videoInteractionRate
     ].join("\t");
 
-    return `${excelRow}`;
+    return `${headers}\n${excelRow}`;
   }
 
   // 显示模态框
@@ -346,3 +358,4 @@
   document.addEventListener('selectstart', function (e) {
     if (isDragging) e.preventDefault();
   });
+})();
