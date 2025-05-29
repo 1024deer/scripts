49a01058648f38ca007906e480874f1f7b761eb4
1024deer <72494945+1024deer@users.noreply.github.com>
2025-05-29 11:05:04 +0800

    Create TK_DM.meta.js

---
+++ TK_DM.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         TK 达人建联DM助手
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  TK 达人建联DM助手，话术存储，辅助回复，达人打标签
+// @match        https://affiliate.tiktokglobalshop.com/seller/im**
+// @match        https://affiliate.tiktokshopglobalselling.com/seller/im**
+// @updateURL    https://raw.githubusercontent.com/1024deer/scripts/main/TK_DM.meta.js
+// @downloadURL  https://raw.githubusercontent.com/1024deer/scripts/main/TK_DM.user.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// ==/UserScript==
