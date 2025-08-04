2c2253a867b7967dda09e6d500a8bb17a5cb9297
1024deer <72494945+1024deer@users.noreply.github.com>
2025-08-04 19:08:07 +0800

    Update influencer_info.meta.js

--- influencer_info.meta.js
+++ influencer_info.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         复制达人信息
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  在网页右侧添加可拖动的悬浮窗，点击显示表格数据、数据块信息和Top视频信息（已移除销售名称和星级功能，整合为统一JSON格式，支持未公开数据处理，货币转换为美元，新增直播数据提取，支持范围格式货币数据）
 // @author       You
 // @match        https://affiliate.tiktokglobalshop.com/connection/creator/detail*
